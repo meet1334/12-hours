6e2eac84086cef94958b53f60c171118a9831b10
meet.vaghasiya <meetvaghasiya66@gmail.com>
2024-03-31 19:21:44 +0530

    Create CRUD operations

--- server.js
+++ server.js
@@ -6,66 +6,64 @@ const morgan = require("morgan");
 const server = express();
 
 const index = fs.readFileSync("index.html", "utf-8");
-const data = fs.readFileSync("product.json", "utf-8");
-
-// MIDDLEWARE
-
-// server.use((req, res, next) => {
-//   console.log(req.method, req.ip, req.hostname, req.get("User-Agent"));
-//   next();
-// });
-
-// use for body payload json access ... before this we can use bodyparser for access this payload
+const data = JSON.parse(fs.readFileSync("product.json", "utf-8"));
 
 server.use(express.json());
 
 // for using to access public files... added files in public folder
 server.use(express.static("public"));
 
-// 3rd party morgan use for middleware
-//dev is mothod for data to get
-// server.use(morgan("dev"));
-server.use(morgan("default"));
-// custome middleware
-
-const auth = (req, res, next) => {
-  console.log(req.query);
-  if (req.body.password === "123") {
-    next();
-  } else {
-    res.sendStatus(401);
-  }
-};
-
-// server.use(auth);
 // API ENDPOINTS
 
-server.get("/product/:id", (req, res) => {
-  console.log(req.params);
-  res.send(req.params);
-});
-server.get("/", auth, (req, res) => {
-  res.send({ type: "GET" });
-});
-server.post("/", auth, (req, res) => {
-  res.json({ type: "post" });
-});
-server.put("/", (req, res) => {
-  res.json({ type: "put" });
-});
-server.delete("/", (req, res) => {
-  res.json({ type: "delete" });
-});
-server.patch("/", (req, res) => {
-  res.json({ type: "patch" });
+// Create post Apis to add products Create POST /products
+
+server.post("/products", (req, res) => {
+  console.log(req.body);
+  data.products.push(req.body);
+  res.status(201).send(req.body);
 });
 
-server.get("/demo", (req, res) => {
-  res.status(300).json(data);
-  // res.sendStatus(400);
-  // res.send("data");
-  // res.sendFile(`E:/Tech/12 hours/12-hours/index.html`);
-  console.log("server is called");
+// Read GET APIS
+server.get("/products", (req, res) => {
+  res.json(data);
+});
+
+server.get("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const product = data.products.find((p) => p.id === id);
+  console.log(product);
+  res.json(product);
+});
+
+// Update  PUT product
+
+server.put("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productId = data.products.findIndex((p) => p.id === id);
+  const products = data.products.splice(productId, 1, { ...req.body, id: id });
+  res.status(201).send({ message: "product updated successfully" });
+});
+
+// Update  PATCH product
+
+server.patch("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productId = data.products.findIndex((p) => p.id === id);
+  const product = data.products[productId];
+  const products = data.products.splice(productId, 1, {
+    ...product,
+    ...req.body,
+  });
+  res.status(201).send({ message: "product updated successfully" });
+});
+
+// Delete  DELETE
+
+server.delete("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productId = data.products.findIndex((p) => p.id === id);
+  const products = data.products.splice(productId, 1);
+  res.status(201).send({ message: "product deleted successfully" });
 });
 
 server.listen(8080, () => {
